bcc5f311891549052d48513ec4ef74e81be897f7
Nicolas <nicolasperez95.a@gmail.com>
2025-07-26 18:47:11 -0300

    feat: added all stuff

---
+++ src/structureAndComments/verticalFormatting.ts
@@ -0,0 +1,34 @@
+/**
+ * This file is part of the Clean Code Handbook.
+ * It is licensed under the MIT License.
+ * Created in 2025
+ * 
+ * 
+ * Vertical Formatting
+ * 
+ * The code should be redable from top to bottom without many jumps or horizantal scrolling.
+ * Obusly, this is hard to react in larger codebases, there always be calls to function, imports, etc
+ * but, a good way is to keep the code as vertical as possible, this means:
+ * - Avoiding long lines of code
+ * - Avoiding horizontal scrolling
+ * - Avoiding too many nested blocks of code
+ * - Avoiding too many imports and exports in the same file
+ * - Avoiding too many comments in the same file
+ * - Avoiding too many functions in the same file
+ * - Avoiding too many classes in the same file
+ * - Avoiding too many variables in the same file
+ * - Avoiding too many constants in the same file
+ * - Avoiding too many types in the same file
+ * - Avoiding too many interfaces in the same file
+ * - Avoiding too many enums in the same file
+ * - Avoiding too many generics in the same file
+ * - Avoiding too many decorators in the same file
+ * - Avoiding too many comments in the same file
+ * 
+ * Ex:
+ */
+
+export function readFileStream(): boolean {
+  // Read some code
+  return true;
+}
